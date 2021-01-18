d2b0bf6a78341e54f60d558e88398de539a737d4
Nick May <nickmay9@outlook.com>
2021-01-18 17:32:52 -0500

    created api calls for get all and get single candidates

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const sqlite3 = require('sqlite3').verbose();
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,17 +8,81 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if(err) {
+        return console.error(err.message);
+    }
+
+    return console.log('Connected to the election database.');
+});
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
 });
 
+//Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//GET a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if(err) {
+            res.send(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
+
+//Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// db.run(sql, params, function(err, result) {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// });
+
 //Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+//Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
