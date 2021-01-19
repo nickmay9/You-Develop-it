88718c5659c55d6e7637fd4393961a32fe8acab7
Nick May <nickmay9@outlook.com>
2021-01-19 16:40:41 -0500

    updated server.js to join tables

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ app.get('/', (req, res) => {
 
 //Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                    AS party_name
+                    FROM candidates
+                    LEFT JOIN parties
+                    ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if(err) {
@@ -43,7 +47,12 @@ app.get('/api/candidates', (req, res) => {
 
 //GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                    AS party_name
+                    FROM candidates
+                    LEFT JOIN parties
+                    ON candidates.party_id = parties.id
+                    WHERE id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if(err) {
